0e35958f457b58d4959e19edcf446fe45702d776
Riya0899 <riyakohli7355@gmail.com>
2025-12-30 22:23:08 +0530

    word replacement

---
+++ file-I/O/p5.py
@@ -0,0 +1,10 @@
+words=["donkey","bad","good"]
+
+with open("text.txt",'r') as f:
+    content=f.read()
+for word in words:
+      
+    content=content.replace(word,"#"*len(word))
+
+with open("text.txt",'w') as f:
+    f.write(content)
