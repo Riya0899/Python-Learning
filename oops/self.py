f50ffde0611c9e4ce76cad3fcfbd76f1c73657ac
Riya0899 <riyakohli7355@gmail.com>
2025-12-31 14:44:03 +0530

    self parameter

---
+++ oops/self.py
@@ -0,0 +1,16 @@
+
+class Employee:
+    language="python"  # this is class artribute
+    salary=120000
+   
+   # self is automatically passed with a function call from an object
+    def getinfo(self):
+        print(f"language is {self.language} and salary is {self.salary}")
+    def greet(self):
+        print("good morning")
+
+obj=Employee()
+obj.getinfo( )
+obj.greet()
+# Employee.getinfo(obj)
+
