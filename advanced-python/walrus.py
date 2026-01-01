759d416c0cc335e21cf334de418c3a39e368597b
Riya0899 <riyakohli7355@gmail.com>
2026-01-01 16:22:38 +0530

    walrus operator

---
+++ advanced-python/walrus.py
@@ -0,0 +1,6 @@
+ # walrus operator
+ 
+if(n:= len([1,2,3,4,5])>3):
+     print(f"list is too long ({n} elements,expected<=3)")
+     
+# it means that output is too long(expected <=3 and there are 5 elements)
